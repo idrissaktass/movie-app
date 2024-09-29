b7811510dba5e4a358a2fa2aaac572f25ca5d369
idrissaktass <idrissaktass98@gmail.com>
2024-09-29 05:02:16 +0300

    new upppghgh

--- backend/api/register.js
+++ backend/api/register.js
@@ -1,5 +1,5 @@
-import dbConnect from '../../utils/dbConnect';
-import User from '../../models/User';
+import dbConnect from '../utils/dbConnect';
+import User from '../models/User';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import Cors from 'cors';
