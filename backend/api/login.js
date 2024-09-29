1ba0f9423a553e2d84839ee8f4bb7f1016640a64
idrissaktass <idrissaktass98@gmail.com>
2024-09-29 03:58:20 +0300

    new upppghgh

--- backend/api/login.js
+++ backend/api/login.js
@@ -4,23 +4,26 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import Cors from 'cors';
 
-const allowedOrigins = [
-    'https://movie-app-front-three.vercel.app', // Production URL
-    'http://localhost:3000', // Local development URL
-  ];
+app.use((req, res, next) => {
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+      'https://movie-app-front-three.vercel.app'
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Private-Network", true);
+    //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+    res.setHeader("Access-Control-Max-Age", 7200);
   
-  const cors = Cors({
-    origin: (origin, callback) => {
-      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: ['GET', 'POST', 'OPTIONS'],
-    credentials: true,
+    next();
   });
-  
 
 const runMiddleware = (req, res, fn) => {
   return new Promise((resolve, reject) => {
