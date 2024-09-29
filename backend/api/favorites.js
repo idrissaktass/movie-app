38d6fc11c5494d505c51d0b6577bb3b520704288
idrissaktass <idrissaktass98@gmail.com>
2024-09-29 04:04:02 +0300

    new upppghgh

--- backend/api/favorites.js
+++ backend/api/favorites.js
@@ -2,26 +2,13 @@ import dbConnect from '../utils/dbConnect';
 import User from '../models/User';
 import Cors from 'cors';
 
-app.use((req, res, next) => {
-    res.setHeader(
-      "Access-Control-Allow-Origin",
-      'https://movie-app-front-three.vercel.app'
-    );
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-    );
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    res.setHeader("Access-Control-Allow-Private-Network", true);
-    //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-    res.setHeader("Access-Control-Max-Age", 7200);
-  
-    next();
+const cors = Cors({
+    origin: 'https://movie-app-front-three.vercel.app',
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
   });
+  
 
 const runMiddleware = (req, res, fn) => {
   return new Promise((resolve, reject) => {
