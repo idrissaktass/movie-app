6e86f230f523b1c74d169d7006f568717a99a615
idrissaktass <80005089+idrissaktass@users.noreply.github.com>
2024-12-06 11:19:35 +0300

    Update register.js

--- backend/api/register.js
+++ backend/api/register.js
@@ -5,7 +5,7 @@ import jwt from 'jsonwebtoken';
 import Cors from 'cors';
 
 const cors = Cors({
-  origin: 'https://movie-app-x.vercel.app',
+  origin: 'https://www.cinescope.online',
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
@@ -28,7 +28,7 @@ export default async function handler(req, res) {
   await runMiddleware(req, res, cors);
 
   if (req.method === 'OPTIONS') {
-    res.setHeader('Access-Control-Allow-Origin', 'https://movie-app-frontend-xi.vercel.app');
+    res.setHeader('Access-Control-Allow-Origin', 'https://www.cinescope.online');
     res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, PUT, PATCH, POST, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     return res.status(204).end();
